859eee74b14d5c0eb21410fa486c5125d9bdf3eb
Luca Gandolfi <gandolfiluca03@gmail.com>
2023-03-03 00:10:13 +0100

    Modified CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,37 +1,27 @@
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	[Header("Position")]
-		public Transform cameraPosition;
-		public GameObject cameraHolder;
-
 	[Header("Orientation")]
-		[SerializeField, Range(1, 10)] private int sensX;
-		[SerializeField, Range(1, 10)] private int sensY;
-		[SerializeField] private bool invertX = false;
-		[SerializeField] private bool invertY = false;
+		[SerializeField, Range(1, 10)] int sensX = 5;
+		[SerializeField, Range(1, 10)] int sensY = 5;
+		[SerializeField] bool invertX = false;
+		[SerializeField] bool invertY = false;
 
 	float xRotation;
 	float yRotation;
-	public Transform orientation;
+	Transform orientation;
 
 	void Start() {
 		Cursor.lockState = CursorLockMode.Locked;
+		orientation = transform.parent.gameObject.transform.parent.gameObject.transform.Find("Orientation");
 	}
 
 	void Update() {
-		cameraHolder.transform.position = cameraPosition.position;
+		if (invertX) sensX *= -1;
+		if (invertY) sensY *= -1;
 
-		if (invertX)
-			sensX *= -1;
-		if (invertY)
-			sensY *= -1;
-		float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * 100;
-		float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * 100;
-
-		yRotation += mouseX;
-		xRotation -= mouseY;
-		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+		yRotation += Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * 100;
+		xRotation = Mathf.Clamp(xRotation - Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * 100, -90f, 90f);
 
 		transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
 		orientation.rotation = Quaternion.Euler(0, yRotation, 0);
