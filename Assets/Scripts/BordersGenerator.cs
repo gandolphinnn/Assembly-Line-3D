0a60c088987f748995ead5677517a8ea28ba8ab7
Luca Gandolfi <gandolfiluca03@gmail.com>
2023-03-06 15:24:48 +0100

    Now uses 2 prefab tiles to make easy block system

--- Assets/Scripts/BordersGenerator.cs
+++ Assets/Scripts/BordersGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BordersGenerator: MonoBehaviour {
+	[SerializeField] GameObject tilePrefab;
+	[SerializeField] GameObject invertPrefab;
 	[SerializeField] Material borderMaterial;
 	[SerializeField] Material terrainMaterial;
 	bool[,] chunks;
@@ -28,8 +30,8 @@ public class BordersGenerator: MonoBehaviour {
 					#region floor and ceiling
 						for (int x = xChunk*chunkDim.x; x < (xChunk + 1)*chunkDim.x; x++) {
 							for (int z = zChunk*chunkDim.z; z < (zChunk + 1)*chunkDim.z; z++) {
-								createTile(ceiling, new Vector3(x+.5f, chunkDim.y, z+.5f), new Vector3(180, 0, 0), $"Ceiling Tile({x},{chunkDim.y},{z})").GetComponent<MeshRenderer>().enabled = false;
-								createTile(floor, new Vector3(x+.5f, 0, z+.5f), new Vector3(), $"Floor Tile({x},0,{z})").GetComponent<MeshRenderer>().material = terrainMaterial;
+								createTile(ceiling, new Vector3(x+.5f, chunkDim.y+.5f, z+.5f), new Vector3(0, -90, 0), true).GetComponent<MeshRenderer>().enabled = false;
+								createTile(floor, new Vector3(x+.5f, -.5f, z+.5f), new Vector3(0, 90, 0)).GetComponent<MeshRenderer>().material = terrainMaterial;
 							}
 						}
 					#endregion
@@ -38,15 +40,7 @@ public class BordersGenerator: MonoBehaviour {
 							GameObject wall = NewEmpty(walls, "North Wall");
 							for (int y = 0; y < chunkDim.y; y++) {
 								for (int x = xChunk*chunkDim.x; x < (xChunk + 1)*chunkDim.x; x++) {
-									createTile(wall, new Vector3(x+.5f, y+.5f, (zChunk+1)*chunkDim.z), new Vector3(-90, 0, 0), $"North Wall Tile({x},{y},{(zChunk+1)*chunkDim.z})");
-								}
-							}
-						}
-						if (zChunk == 0 || !chunks[xChunk, zChunk-1]) {
-							GameObject wall = NewEmpty(walls, "South Wall");
-							for (int y = 0; y < chunkDim.y; y++) {
-								for (int x = xChunk*chunkDim.x; x < (xChunk + 1)*chunkDim.x; x++) {
-									createTile(wall, new Vector3(x+.5f, y+.5f, zChunk*chunkDim.z), new Vector3(90, 0, 0), $"South Wall Tile({x},{y},{zChunk*chunkDim.z})");
+									createTile(wall, new Vector3(x+.5f, y+.5f, (zChunk+1)*chunkDim.z+.5f), new Vector3(-90, 0, 0));
 								}
 							}
 						}
@@ -54,7 +48,15 @@ public class BordersGenerator: MonoBehaviour {
 							GameObject wall = NewEmpty(walls, "East Wall");
 							for (int y = 0; y < chunkDim.y; y++) {
 								for (int z = zChunk*chunkDim.z; z < (zChunk + 1)*chunkDim.z; z++) {
-									createTile(wall, new Vector3((xChunk+1)*chunkDim.x, y+.5f, z+.5f), new Vector3(0, 0, 90), $"East Wall Tile({(xChunk+1)*chunkDim.x},{y},{z})");
+									createTile(wall, new Vector3((xChunk+1)*chunkDim.x+.5f, y+.5f, z+.5f), new Vector3(0, 0, -90), true);
+								}
+							}
+						}
+						if (zChunk == 0 || !chunks[xChunk, zChunk-1]) {
+							GameObject wall = NewEmpty(walls, "South Wall");
+							for (int y = 0; y < chunkDim.y; y++) {
+								for (int x = xChunk*chunkDim.x; x < (xChunk + 1)*chunkDim.x; x++) {
+									createTile(wall, new Vector3(x+.5f, y+.5f, zChunk*chunkDim.z-.5f), new Vector3(90, 0, 0));
 								}
 							}
 						}
@@ -62,7 +64,7 @@ public class BordersGenerator: MonoBehaviour {
 							GameObject wall = NewEmpty(walls, "West Wall");
 							for (int y = 0; y < chunkDim.y; y++) {
 								for (int z = zChunk*chunkDim.z; z < (zChunk + 1)*chunkDim.z; z++) {
-									createTile(wall, new Vector3(xChunk*chunkDim.x, y+.5f, z+.5f), new Vector3(0, 0, -90), $"West Wall Tile({xChunk*chunkDim.x},{y},{z})");
+									createTile(wall, new Vector3(xChunk*chunkDim.x-.5f, y+.5f, z+.5f), new Vector3(0, 0, 90), true);
 								}
 							}
 						}
@@ -74,33 +76,17 @@ public class BordersGenerator: MonoBehaviour {
 		}
 		return true;
 	}
-	GameObject createTile(GameObject parent, Vector3 coord, Vector3 rotation, string name) {
-		#region mesh definition
-			Mesh mesh = new Mesh();
-			Vector3[] vertices = new Vector3[] {
-				new Vector3(-.5f, 0, -.5f),
-				new Vector3(-.5f, 0, .5f),
-				new Vector3(.5f, 0, -.5f),
-				new Vector3(.5f, 0, .5f),
-			};
-			mesh.vertices = vertices;
-			int[] triangles = new int[] {
-				0, 1, 2,
-				1, 3, 2
-			};
-			mesh.triangles = triangles;
-		#endregion
-		GameObject tile = NewEmpty(parent, name);
-		tile.layer = LayerMask.NameToLayer("Map Borders");
-		tile.transform.position = coord;
-		tile.AddComponent<MeshFilter>();
-		tile.AddComponent<MeshRenderer>();
+	GameObject createTile(GameObject parent, Vector3 coord, Vector3 rotation, bool invert = false) {
+		GameObject tile;
+		if (invert)
+			tile = Instantiate(invertPrefab, coord, Quaternion.Euler(rotation), parent.transform);
+		else
+			tile = Instantiate(tilePrefab, coord, Quaternion.Euler(rotation), parent.transform);
+		tile.layer = LayerMask.NameToLayer("Borders");
 		tile.AddComponent<MeshCollider>();
-		tile.GetComponent<MeshFilter>().mesh = mesh;
 		tile.GetComponent<MeshRenderer>().material = borderMaterial;
-		tile.GetComponent<MeshRenderer>().enabled = true;
-		tile.GetComponent<MeshCollider>().sharedMesh = mesh;
-		tile.transform.Rotate(rotation);
+		tile.GetComponent<MeshCollider>();
+		tile.name = $"{parent.name} Tile({Mathf.FloorToInt(coord.x)},{Mathf.FloorToInt(coord.y)},{Mathf.FloorToInt(coord.z)})";
 		return tile;
 	}
 	GameObject NewEmpty(GameObject parent, string name) {
